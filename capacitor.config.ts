0eff5602f6b95be381045edb38b6ca1b33fbf409
Paul Golmann <mail@pje-web.de>
2023-02-05 14:53:48 +0100

    setup capacitor (ios, android)

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.alzhmeimerniedersachsen.auguste',
+  appName: 'Auguste',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
